26dbace077d54be7e40d1c7138862775db738151
Poojavpatel <32796407+Poojavpatel@users.noreply.github.com>
2019-05-12 22:37:17 +0530

    q4 palindrome done

--- 4_palindrome.js
+++ 4_palindrome.js
@@ -1,8 +1,10 @@
 const readline = require('readline').createInterface({input: process.stdin,output: process.stdout})
 
-readline.question(`Enter a scentence to check if its a palindrome or not:   `, (scentence) => {
-    
-    result='not a'
+readline.question(`Enter a scentence to check if its a palindrome or not:`, (scentence) => {
+    s1 = scentence.replace(/\s/g,'').trim().toLowerCase();
+    const rev = s1.split("").reverse().join("");
+    if(s1==rev){result='a'}
+    else{result='not a'}
     console.log(`The scentence "${scentence}" is ${result} palindrome`);
     readline.close()
 })
